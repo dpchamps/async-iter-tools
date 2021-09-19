47e787a6ee36677b0173a58fbe3b7745e8b6ed13
dpchamps <me@davecampion.com>
2021-09-18 20:51:04 -0700

    pushToPull

---
+++ src/pushToPull.ts
@@ -0,0 +1,35 @@
+interface InvertedIterator<T>{
+    next: (v: T) => void,
+    return: (v: T) => void,
+    throw: (reason?: unknown) => void
+}
+
+
+export async function* pushToPull<T>(
+    onIterationStep: (cf: InvertedIterator<T>) => void
+): AsyncGenerator<T, undefined, unknown> {
+    let res = (_v: IteratorResult<T>|PromiseLike<IteratorResult<T>>) => {};
+    let rej = (_reason?: unknown) => {};
+
+    onIterationStep({
+        next: (value) => res({value, done: false}),
+        return: (value) => res({value, done: true}),
+        throw: (e?: any) => rej(e)
+    });
+
+    while(true){
+        try{
+            const {value, done} = await new Promise<IteratorResult<T>>((resolve, reject) => {
+                res = resolve;
+                rej = reject;
+            });
+
+            yield value;
+
+            if(done) return;
+        }catch(e){
+            throw e;
+        }
+
+    }
+}
