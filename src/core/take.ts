14c2c5801884c1cf502468e78607c4c33a390262
David Campion <David.Campion@walmartlabs.com>
2021-11-27 15:33:22 -0800

    add take

---
+++ src/core/take.ts
@@ -0,0 +1,12 @@
+import {intoAsyncIterableIterator} from "../utils/intoAsyncIterableIterator";
+import {InferAsyncIterator, RequiredAsyncIterableIterator} from "../utility-types";
+import {iteratorResult} from "../utils/iterator-result";
+
+export const take = <T extends AsyncIterator<any>>(it:T, n: number): RequiredAsyncIterableIterator<InferAsyncIterator<T>> => ({
+ ...intoAsyncIterableIterator(it),
+ async next(...args){
+     if(--n < 0) return iteratorResult(undefined, true);
+
+     return it.next(...args);
+ }
+})
